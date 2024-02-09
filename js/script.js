e09c1d1bc85006c53fb14aacda6938f8fb5aa422
jennifer cristine <107707471+jennifer-cristine12@users.noreply.github.com>
2024-02-09 17:57:35 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 //seletores
 const novoCancelar = document.querySelector("#new");
-const form = document.querySelector("form");
+const form = document.querySelector("div");
 const cadastrar = document.querySelector("#criar");
 const tbody = document.querySelector("tbody");
+const btnExcluir = document.querySelectorAll(".remover");
+const inputNome = document.querySelector("#Nome");
+const inputData = document.querySelector("#Data");
+const btnEditar = document.querySelectorAll(".editar");
 //eventos
 novoCancelar.addEventListener("click", () => {
   tog();
@@ -10,22 +14,32 @@ novoCancelar.addEventListener("click", () => {
 cadastrar.addEventListener("click", () => {
   novo();
 });
+
 //funcoes
 function novo() {
   const data = document.createElement("tr");
   data.innerHTML = `
-<td>08/02/2024 17:01</td>
-<td>Campeões da fiel</td>
+<td>${inputData.value}</td>
+<td>${inputNome.value}</td>
 <td class="optTd">
-  <button class="opt" class="editar">
+  <button class="opt" class="editar" onClick="editar(this)">
     <i class="fa-solid fa-pen"></i>
   </button>
-  <button class="opt" class="remover">
+  <button class="opt" class="remover" onClick="remover(event)">
     <i class="fa-solid fa-trash"></i>
   </button>
 </td>
 `;
   tbody.appendChild(data);
+
+  inputData.value = "";
+  inputNome.value = "";
+  inputNome.focus();
+}
+function remover(event) {
+  const targetEl = event.target;
+  const parentEl = targetEl.closest("tr");
+  parentEl.remove();
 }
 function tog() {
   //mostra e esconde o formulario
@@ -33,4 +47,31 @@ function tog() {
   form.classList.contains("hide")
     ? (novoCancelar.textContent = "Novo time")
     : (novoCancelar.textContent = "Cancelar");
+}
+function editar(button) {
+  //altera o nome do time
+  const row = button.closest("tr"); //assim como o remove, ele pega o elemento de linha mais proximo
+  const cells = row.querySelectorAll("td"); //pega a celula mais proxima
+  const nameCell = cells[1]; //pega o nome
+  const dataCell = cells[0]; //pega a data
+  const nameInput = document.createElement("input");
+
+  const dataInput = document.createElement("input"); //cria um input por cima da celula
+  nameInput.type = "text"; //tipo de input
+  dataInput.type = "date";
+  nameInput.value = nameCell.textContent;
+  dataInput.value = dataCell.textContent; //coloca o valor do input para o nome
+  nameCell.textContent = "";
+  dataCell.textContent = "";
+  dataCell.appendChild(dataInput);
+  nameCell.appendChild(nameInput);
+  nameInput.focus();
+  dataInput.focus();
+
+  nameInput.addEventListener("blur", () => {
+    //quando tirar o foco, o nome volta ao normal(sem input)
+    nameCell.textContent = nameInput.value;
+
+    dataCell.textContent = dataInput.value;
+  });
 }
